9ca57ff3e16025e07dc0695d29de4d91c1ce9d87
Dan Waseen <dan.waseen1@gmail.com>
2018-01-30 18:57:06 -0600

    Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Dan Waseen on 1/30/18.
+//  Copyright © 2018 RevWon. All rights reserv...ed.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
